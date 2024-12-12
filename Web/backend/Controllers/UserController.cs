74379bbc25d91cd3beefdf6e31a5c00095ae6a8d
Balogh Tamás <balogh.tamas2@burg.lo.hu>
2024-12-12 14:33:22 +0100

    Added backend

---
+++ Web/backend/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Findgroup_Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Findgroup_Backend.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        ApplicationDbContext _context;
+       public ActionResult GetUsers()
+       {
+            return Ok();
+       }
+    }
+}
---
+++ Web/backend/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Findgroup_Backend.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
