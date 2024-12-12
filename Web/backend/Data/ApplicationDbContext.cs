9b33484a32547ebf2813fdb60e2bcc5ccc996a70
Ordinary56 <tbalogh695@gmail.com>
2024-12-12 18:55:42 +0100

    Create basic REST API for users

--- Web/backend/Data/ApplicationDbContext.cs
+++ Web/backend/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 namespace Findgroup_Backend.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public partial class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
---
+++ Web/backend/Migrations/20241212174046_init_00001.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Findgroup_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_00001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
