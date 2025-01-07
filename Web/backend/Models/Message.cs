f9f03287ae0798acf69f3b149edd1a59642e926e
Ordinary56 <71382932+Ordinary56@users.noreply.github.com>
2025-01-07 20:13:26 +0100

    started working on databas (#8)

--- Web/backend/Models/Message.cs
+++ Web/backend/Models/Message.cs
@@ -1,10 +1,20 @@
-﻿namespace Findgroup_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Findgroup_Backend.Models
 {
     public class Message
     {
+
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
         public int RoomId { get; set; }
+
+        [Required]
         public string Content { get; set; } = "";
 
     }
--- Web/backend/Models/Post.cs
+++ Web/backend/Models/Post.cs
@@ -7,8 +7,10 @@
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime UpdateDate { get; set; }
         public string UserId { get; set; }
-        public int TopicId { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
+        public User User { get; set; } = default!;
+        public IList<Tag> Tags { get; set; } = [];
+        
 
 
     }
