8f4ce4a24134f91b83681a8e460de7c884d344f9
Placko135 <papplaszlolevente@gmail.com>
2025-01-10 13:03:43 +0100

    started making the main page

--- Web/frontend/src/component/pages/Home.tsx
+++ Web/frontend/src/component/pages/Home.tsx
@@ -1,12 +1,10 @@
 import styles from "./module.css/home.module.css"
-
+import testkep from "../../assets/test.png"
 const Home = () => {
   return (
     <>
-    <div className={styles.container}>
-    <h1>FIND A GROUP FOR YOU</h1>
-    <br/>
-    <h2>Join to them and level up!</h2>
+    <div className={styles.gamechooser}>
+        <button className={styles.game}></button>
     </div>
     </>
   )
