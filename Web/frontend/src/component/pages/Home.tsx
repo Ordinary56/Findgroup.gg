92d905ee7d95567a536d34f932afc2d05a41d287
Placko135 <papplaszlolevente@gmail.com>
2025-01-23 11:47:32 +0100

    fixed material ui (janky way)

--- Web/frontend/src/component/pages/Home.tsx
+++ Web/frontend/src/component/pages/Home.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import styles from "./module.css/home.module.css"
-
+import clsx from 'clsx';
 export default function Home() {
   const [game, setgame] = React.useState('League of legends');
 
@@ -15,11 +15,11 @@ export default function Home() {
 
   return (
     <ToggleButtonGroup
-    className={styles.gamechooser}
+    className={clsx(styles.gamechooser)}
       value={game}
         onChange={handleChange}>
-      <ToggleButton value="League of legends" className={styles.gamechooserItem}>League of legends</ToggleButton>
-      <ToggleButton value="Apex legends" className={styles.gamechooserItem} >Apex legends</ToggleButton>
+      <ToggleButton  value="League of legends" className={clsx(styles.gamechooserItem)}>League of legends</ToggleButton>
+      <ToggleButton value="Apex legends" className={clsx(styles.gamechooserItem)} >Apex legends</ToggleButton>
 
     </ToggleButtonGroup>
   );
