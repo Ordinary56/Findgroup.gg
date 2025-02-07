4d5c69399650776c51f80594405f647941e4e269
Ordinary56 <balogh.tamas2@burg.lo.hu>
2025-02-07 14:28:51 +0100

    Started working on Group creation UI

--- Web/frontend/src/component/pages/CreateGroup.tsx
+++ Web/frontend/src/component/pages/CreateGroup.tsx
@@ -2,8 +2,10 @@ import styles from "./module.css/creategroup.module.css";
 import BackToHomeButton from "../Back_To_Home_Button/Back_to_Home";
 import { Link } from "react-router-dom"
 import { ROUTES } from "../../App"
+import { useState } from "react";
 
 const CreateGroup = () => {
+  const [postName, setPostName] = useState<string>("");
   return (<>
     <div className={styles.Back_to_Home}>
       <BackToHomeButton />
@@ -14,7 +16,7 @@ const CreateGroup = () => {
       <div className={styles.very_basic_details}>
         <div className={styles.details_inputs}>
           <form>
-            <input type="text" placeholder="Group Name" />
+            <input type="text" placeholder="Group Name" onInput={e => setPostName(prev => prev = e.target.value)} />
             <input type="text" placeholder="Group Description" />
             <select>
               <option value="">Select Option</option>
@@ -26,24 +28,24 @@ const CreateGroup = () => {
         </div>
 
       </div>
-        <div className={styles.preview}>
+      <div className={styles.preview}>
 
-          <div className={styles.title_wrapper}>
-            <h1 className={styles.title}>Users's game</h1>
-          </div>
-
-          <div className={styles.title_and_team_size}>
-            <h3 className={styles.creatorname}>User</h3>
-            <span className={styles.team_size}>Team size: 20</span>
-          </div>
-
-          <div className={styles.tags}>
-            <div>Competitive</div>
-          </div>
-          <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu est eros. Suspendisse vitae tortor id sem tempor rutrum. In tempus ante vel sapien ornare maximus. Nam varius est risus, a ullamcorper tortor consectetur ac. Phasellus sit amet efficitur augue. Mauris ornare arcu vel nisl commodo iaculis. Pellentesque congue justo nec tellus varius commodo. Vestibulum ac volutpat quam. Sed quis purus auctor, scelerisque tortor vel, fermentum ex. Aenean eget turpis finibus, scelerisque nibh vel, pulvinar est. Ut rutrum egestas venenatis. Nullam eu interdum enim, ac aliquet est. In auctor efficitur commodo. Suspendisse potenti. Ut ante orci, dictum in mollis vel.</div>
+        <div className={styles.title_wrapper}>
+          <h1 className={styles.title}>Users's game</h1>
         </div>
 
-    <button className={styles.create_listing}> <Link to={ROUTES.aftercreate.path}>Create Listing</Link></button>
+        <div className={styles.title_and_team_size}>
+          <h3 className={styles.creatorname}>User</h3>
+          <span className={styles.team_size}>Team size: 20</span>
+        </div>
+
+        <div className={styles.tags}>
+          <div>Competitive</div>
+        </div>
+        <textarea className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu est eros. Suspendisse vitae tortor id sem tempor rutrum. In tempus ante vel sapien ornare maximus. Nam varius est risus, a ullamcorper tortor consectetur ac. Phasellus sit amet efficitur augue. Mauris ornare arcu vel nisl commodo iaculis. Pellentesque congue justo nec tellus varius commodo. Vestibulum ac volutpat quam. Sed quis purus auctor, scelerisque tortor vel, fermentum ex. Aenean eget turpis finibus, scelerisque nibh vel, pulvinar est. Ut rutrum egestas venenatis. Nullam eu interdum enim, ac aliquet est. In auctor efficitur commodo. Suspendisse potenti. Ut ante orci, dictum in mollis vel.</textarea>
+      </div>
+
+      <button className={styles.create_listing}> <Link to={ROUTES.aftercreate.path}>Create Listing</Link></button>
     </div>
   </>
   );
