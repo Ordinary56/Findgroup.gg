4645ae8d21d2068826d6b56d37ffb29fae2afee2
Placko135 <papplaszlolevente@gmail.com>
2025-02-12 11:24:27 +0100

    removed unnecesary pics

--- Web/frontend/src/component/pages/ProfilePage.tsx
+++ Web/frontend/src/component/pages/ProfilePage.tsx
@@ -2,6 +2,7 @@ import styles from "./module.css/profile.module.css";
 import { useParams, Link } from "react-router-dom";
 import { User } from "../../api/Models/User";
 import { ROUTES } from "../../App";
+import pfp from "../../../public/Thepic.png"
 
   
   type UserDetailsPageProps = {
@@ -16,10 +17,17 @@ const UserDetailsPage = ({users}:UserDetailsPageProps) => {
       return <p>User cannot be found.</p>;
     }
 
-
+//TO THE PAGE THAT WILL OPEN THIS
+/*
+ onClick={(e) => {
+    e.preventDefault(); // Prevent React Router from handling it
+    window.open(ROUTES.aftercreate.path, "_blank");
+  }}
+*/
   return (
     <div className="user-details">
       <h1>{user.userName}</h1>
+      <img src={pfp} alt="User pfp"/>
     <Link to={ROUTES.aftercreate.path}>Back to previous page</Link>
     </div>
   );
