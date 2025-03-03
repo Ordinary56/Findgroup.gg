7a1d25014e551ed5b2ad6b4ba79f1c675f621642
Placko135 <papplaszlolevente@gmail.com>
2025-03-03 14:12:51 +0100

    Changed group tsx and removed 2 files also started fixing css

--- Web/frontend/src/App.tsx
+++ Web/frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, BrowserRouter as Router, Routes, useLocation, Navigate, Outlet } from "react-router-dom";
 import { useAuth, AuthProvider } from "./component/Auth_Context/AuthContext"; // Authentication logic
 import Navbar from "./component/Navbar/Navbar";
-
 import LandingPage from "./component/pages/Landing_Page/LandingPage";
 import LoginPage from "./component/pages/Login/Login";
 import Home from "./component/pages/Home/Home";
 import RegisterPage from "./component/pages/Register/Register";
 import CreateButton from "./component/Create_group_button/Create_group_button";
 import CreateGroup from "./component/pages/Group_Creation/CreateGroup";
-import CreatorScreenAfterListing from "./component/pages/After_Creation/CreatorScreenAfterListnig";
 import Post from "./component/pages/Post/Post";
 import Group from "./component/pages/Group/Group";
 
@@ -19,7 +17,6 @@ export const ROUTES = {
   login: { path: "/login", title: "Login" },
   create: { path: "/create", title: "Create" },
   landingpage:{ path: "/landingpage", title: "LandingPage" },
-  aftercreate:{path:"/aftercreate", title:"CreatorScreenAfterListing"},
   post: {path : "/post", title : "View Post"},
   group : {path : "/group/:groupId", title: "Group"}
 };
@@ -52,7 +49,6 @@ const AppContent = () => {
           <Route index element={<Home />} />
           <Route path={ROUTES.homepage.path} element={<Home />} />
           <Route path={ROUTES.create.path} element={<CreateGroup />} />
-          <Route path={ROUTES.aftercreate.path} element={< CreatorScreenAfterListing/>} />
           <Route path={ROUTES.post.path} element={<Post/>} />
           <Route path={ROUTES.group.path} element={<Group/>}/>
         </Route>
